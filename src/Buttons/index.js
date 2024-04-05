fe13738b3755f3cb7853899fba3c50333f7f013a
Alina Apaydin <alina.apaydin@gmail.com>
2024-04-05 10:36:27 +0300

    needless row delited

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import "./style.css";
 
 const Buttons = ({tasks, hideDoneTasks}) => {
@@ -9,23 +8,11 @@ const Buttons = ({tasks, hideDoneTasks}) => {
 
     return (
         <div className="task__newButtons">
-        <button className="task__hideOrShowAllDone ">
-            {hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone
-            </button>
-            <button className="task__markAllDone"
-            disabled={tasks.every(({done}) => done)}
-             >
-            Ukończ wszystkie
-            </button>
-            </div>
+        <button className="task__hideOrShowAllDone ">{hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone </button>
+        <button className="task__markAllDone"disabled={tasks.every(({done}) => done)}> Ukończ wszystkie</button>
+        </div>
     );
 };
     
-
-
-
-    
-    <div classNameName="buttons"></div>
-
 
 export default Buttons;
